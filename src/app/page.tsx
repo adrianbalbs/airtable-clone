dc13c7f1dd2aa6fa5af7b708a2f4d44aff4fb5da
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-03-17 19:54:25 +1100

    integrated nextauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import { Bell, BookOpen, ChevronDown, ChevronRight, CircleHelp, Grid2x2, Menu, Plus, Search, ShoppingBag, Upload } from "lucide-react";
+import { Bell, BookOpen, ChevronDown, ChevronRight, CircleHelp, Grid2x2, LogOut, Menu, Plus, Search, ShoppingBag, Upload } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { auth } from "~/server/auth";
+import { auth, signOut } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import { MenuButton, Menu as HeadlessMenu, MenuItems, MenuItem, MenuSeparator } from "@headlessui/react";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-    //const session = await auth();
+    const session = await auth();
 
-    //if (session?.user) {
-    //    void api.post.getLatest.prefetch();
-    //}
+    if (!session?.user) {
+        redirect("api/auth/signin");
+    }
 
     return (
         <HydrateClient>
@@ -34,9 +36,30 @@ export default async function Home() {
                     <div className="w-[30px] h-[30px] flex items-center hover:bg-slate-200 cursor-pointer justify-center border border-slate-300 rounded-full mr-5">
                         <Bell size={15} />
                     </div>
-                    <div className="w-[30px] h-[30px] flex items-center justify-center bg-primary border border-white rounded-full cursor-pointer">
-                        <p>A</p>
-                    </div>
+                    <HeadlessMenu>
+                        <MenuButton className="w-[30px] h-[30px] flex items-center justify-center bg-primary border border-white rounded-full cursor-pointer">
+                            <p>A</p>
+                        </MenuButton>
+                        <MenuItems anchor="bottom end" className="w-[296px] h-36 bg-white z-10 mt-1 border border-slate-300 rounded-xl shadow-lg p-4">
+                            <MenuItem>
+                                <div className="pt-1 pb-2">
+                                    <p className="block text-sm font-semibold">
+                                        Adrian Balbalosa
+                                    </p>
+                                    <span className="text-gray-700 text-sm">adrianbalbs@gmail.com</span>
+                                </div>
+                            </MenuItem>
+                            <MenuSeparator className="my-2 h-px bg-slate-300" />
+                            <MenuItem>
+                                <Link href="/api/auth/signout" className="flex w-full hover:bg-slate-200 text-sm cursor-pointer rounded-md p-2 items-center">
+                                    <LogOut size={15} className="mr-2" />
+                                    <p>
+                                        Logout
+                                    </p>
+                                </Link>
+                            </MenuItem>
+                        </MenuItems>
+                    </HeadlessMenu>
                 </div>
             </header>
             <div className="sticky flex min-h-screen w-full overflow-auto text-black pt-14">
