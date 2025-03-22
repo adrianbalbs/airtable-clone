64a9d19ff0d52bbba451125f3318605c34fdab28
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-03-22 12:55:11 +1100

    fix distinctOn error

--- src/server/api/routers/base.ts
+++ src/server/api/routers/base.ts
@@ -1,77 +1,105 @@
-import { bases } from "~/server/db/schema";
+import { bases, columns, rows, tables } from "~/server/db/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
 import { and, desc, eq } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 
 export const baseRouter = createTRPCRouter({
-    getAllBasesByUser: protectedProcedure.query(async ({ ctx }) => {
-        return await ctx.db
-            .select()
-            .from(bases)
-            .where(eq(bases.createdById, ctx.session.user.id))
-            .orderBy(desc(bases.updatedAt));
+  getAllBasesByUser: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db
+      .selectDistinctOn([bases.id], {
+        id: bases.id,
+        name: bases.name,
+        table: tables.id,
+        createdById: bases.createdById,
+        createdAt: bases.createdAt,
+        updatedAt: bases.updatedAt
+      })
+      .from(bases)
+      .innerJoin(tables, eq(bases.id, tables.base))
+      .where(eq(bases.createdById, ctx.session.user.id))
+      .orderBy(desc(bases.id), desc(bases.updatedAt));
+  }),
+  getBaseById: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const { id } = input;
+      const base = await ctx.db.query.bases.findFirst({
+        where: eq(bases.id, id),
+      });
+      if (!base) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+      return base;
     }),
-    getBaseById: protectedProcedure
-        .input(z.object({ id: z.number() }))
-        .query(async ({ ctx, input }) => {
-            const { id } = input;
-            const base = await ctx.db.query.bases.findFirst({
-                where: eq(bases.id, id),
-            });
-            if (!base) {
-                throw new TRPCError({ code: "NOT_FOUND" });
-            }
-            return base;
-        }),
-    createBase: protectedProcedure
-        .input(z.object({ name: z.string().min(6) }))
-        .mutation(async ({ ctx, input }) => {
-            const { name } = input;
-            const [base] = await ctx.db
-                .insert(bases)
-                .values({ name, createdById: ctx.session.user.id })
-                .returning();
+  createBase: protectedProcedure
+    .input(z.object({ name: z.string().min(6) }))
+    .mutation(async ({ ctx, input }) => {
+      const { name } = input;
 
-            if (!base) {
-                throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" })
-            }
+      const base = await ctx.db.transaction(async (tx) => {
+        const [base] = await tx
+          .insert(bases)
+          .values({ name, createdById: ctx.session.user.id })
+          .returning();
+        if (!base) {
+          throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" })
+        }
+        const [table] = await tx.insert(tables).values({ name: "Table 1", base: base.id }).returning({ id: tables.id })
+        if (!table) {
+          throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" })
+        }
 
-            return base;
-        }),
-    deleteBase: protectedProcedure
-        .input(z.object({ id: z.number() }))
-        .mutation(async ({ ctx, input }) => {
-            const { id } = input;
-            const user = ctx.session.user.id;
-            const base = await ctx.db.query.bases.findFirst({
-                where: and(eq(bases.id, id), eq(bases.createdById, user)),
-            });
-            if (!base) {
-                throw new TRPCError({ code: "NOT_FOUND" });
-            }
-            await ctx.db
-                .delete(bases)
-                .where(and(eq(bases.id, id), eq(bases.createdById, user)));
-        }),
-    updateBase: protectedProcedure
-        .input(z.object({ id: z.number(), name: z.string().min(6) }))
-        .mutation(async ({ ctx, input }) => {
-            const { id, name } = input;
-            const user = ctx.session.user.id;
-            const base = await ctx.db.query.bases.findFirst({
-                where: and(eq(bases.id, id), eq(bases.createdById, user)),
-            });
-            if (!base) {
-                throw new TRPCError({ code: "NOT_FOUND" });
-            }
-            if (base.createdById !== user) {
-                throw new TRPCError({ code: "FORBIDDEN" });
-            }
-            return await ctx.db
-                .update(bases)
-                .set({ name })
-                .where(and(eq(bases.id, id), eq(bases.createdById, user)))
-                .returning();
-        }),
+        await tx.insert(columns).values([
+          { name: "Name", type: "text", table: table.id },
+          { name: "Notes", type: "text", table: table.id },
+          { name: "Assignee", type: "text", table: table.id },
+          { name: "Status", type: "text", table: table.id }])
+
+        await tx.insert(rows).values([
+          { table: table.id },
+          { table: table.id },
+          { table: table.id },
+          { table: table.id },
+        ]);
+        return { ...base, table: table.id };
+      })
+
+      return base;
+    }),
+  deleteBase: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+      const user = ctx.session.user.id;
+      const base = await ctx.db.query.bases.findFirst({
+        where: and(eq(bases.id, id), eq(bases.createdById, user)),
+      });
+      if (!base) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+      await ctx.db
+        .delete(bases)
+        .where(and(eq(bases.id, id), eq(bases.createdById, user)));
+    }),
+  updateBase: protectedProcedure
+    .input(z.object({ id: z.number(), name: z.string().min(6) }))
+    .mutation(async ({ ctx, input }) => {
+      const { id, name } = input;
+      const user = ctx.session.user.id;
+      const base = await ctx.db.query.bases.findFirst({
+        where: and(eq(bases.id, id), eq(bases.createdById, user)),
+      });
+      if (!base) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+      if (base.createdById !== user) {
+        throw new TRPCError({ code: "FORBIDDEN" });
+      }
+      return await ctx.db
+        .update(bases)
+        .set({ name })
+        .where(and(eq(bases.id, id), eq(bases.createdById, user)))
+        .returning();
+    }),
 });
